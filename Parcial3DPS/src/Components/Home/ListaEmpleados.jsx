5d40b652cdbe478879d1ccda4d3a5805199da0ed
Oscar Guillermo Serpas Osegueda <platinumsoul98@hotmail.com>
2020-11-11 18:10:17 -0600

    Pantalla de inicio, hosting

--- Parcial3DPS/src/Components/Home/ListaEmpleados.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const ListaEmpleados = () => (
-    <React.Fragment> 
-        <p>Lista de empleados</p>
-        <p></p>
-    </React.Fragment>
-)
-export default ListaEmpleados
